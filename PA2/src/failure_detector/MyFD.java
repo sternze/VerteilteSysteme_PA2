7037d81c4ce5641d85d262311939e2943e9e707d
Daniel <sternze@gmail.com>
2013-05-22 19:53:52 +0200

    test

--- PA2/src/failure_detector/MyFD.java
+++ PA2/src/failure_detector/MyFD.java
@@ -13,7 +13,7 @@ public class MyFD {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stubdkjfalödskf
 
 	}
 	
